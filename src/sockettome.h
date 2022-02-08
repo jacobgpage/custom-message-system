6017a615187b43fa5c9fe5b4885570bad5d41ec6
jacobgpage <63179397+jacobgpage@users.noreply.github.com>
2022-02-08 18:06:05 -0500

    Delete sockettome.h

--- src/sockettome.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOCKETTOME_H_
-#define SOCKETTOME_H_
-
-int serve_socket(int port);
-int accept_connection(int serverfd);
-int request_connection(char *hn, int port);
-
-#endif // SOCKETTOME_H_
